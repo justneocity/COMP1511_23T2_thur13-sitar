b4a35b5e37ca88cd90fd69329c299853d2c68450
Rifa Jamal <z5311190@ad.unsw.edu.au>
2023-06-08 12:31:35 +1000

    week 1

---
+++ week01/hello_world.c
@@ -0,0 +1,29 @@
+// Basic Hello World program
+// Marc Chee, September 2020
+
+/*
+~ ~
+0 0
+ o
+ -
+\_/
+*/
+
+#include <stdio.h>
+
+int main (void) {
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o \n");
+    printf(" - \n");
+    printf("\\_/\n");
+
+    printf(
+        "~ ~\n"
+        "0 0\n"
+        " o \n"
+        " - \n"
+    );
+    
+    return 0;
+}
